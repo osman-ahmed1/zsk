785079964968930b9ca9f508b431692f63e3d8da
Ahmed Osman <90324063+osman-ahmed1@users.noreply.github.com>
2021-11-10 14:50:02 +0100

    Add files via upload

--- 9_bazy_PHP_delete_insert/skrypty/delete.php
+++ 9_bazy_PHP_delete_insert/skrypty/delete.php
@@ -4,9 +4,10 @@ if(isset($_GET['id'])){
   $connect->query("DELETE FROM user WHERE user_id=$_GET[id]");
   echo "Usunięto ", $connect->affected_rows, " rekordów <br>";
   if($connect->affected_rows)
-    header("location: ../tabela.php?error=0&deleted=$_GET[id]");
+    header("location: ../tabela.php?error_id=0&deleted=$_GET[id]");
   else
-    header("location: ../tabela.php?error=1");
+    header("location: ../tabela.php?error_id=1");
+
 }else{
   header("location: ../tabela.php");
 }
